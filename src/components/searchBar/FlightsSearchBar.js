e726989144627e81e7e222fd532ec3d04c36f688
Michelle <cadavidcarmona1@gmail.com>
2024-12-15 15:58:30 -0500

    first commit

--- src/components/searchBar/FlightsSearchBar.js
+++ src/components/searchBar/FlightsSearchBar.js
@@ -1,14 +1,18 @@
-import React from "react";
-import { Box, TextField, Select, MenuItem, Button, IconButton, InputAdornment } from "@mui/material";
+import React, { useState } from "react";
+import { Box, TextField, Select, MenuItem, Button, IconButton, InputAdornment, Divider } from "@mui/material";
 import SwapHorizIcon from "@mui/icons-material/SwapHoriz";
 import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 import SearchIcon from "@mui/icons-material/Search";
 import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import CircleIconOutlined from '@mui/icons-material/CircleOutlined';
+import LocationOnIconOutlined from '@mui/icons-material/LocationOnOutlined';
 import { theme } from "../../styles/theme";
 export default function FlightSearchBar() {
+    const [isDateFieldActive, setIsDateFieldActive] = useState(false);
+
     return (
-        <Box sx={{ position: "relative" }}>
+        <Box sx={{ position: "relative", alignItems: 'center', justifyContent: 'center',}}>
 
             <Box sx={{
                 width: '100%',
@@ -19,12 +23,24 @@ export default function FlightSearchBar() {
                 gap: 1,
                 flexWrap: "wrap",
                 padding: "10px 10px 40px 10px",
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                mr: '4rem',
+                ml: '4rem',
+                flexDirection: 'column',
             }}>
                 {/* Round Trip Selector */}
 
                 <Select
+                    sx={{
+                        alignSelf: 'start',
+                        justifySelf: 'start',
+                        mr: 'auto',
+                        padding: '0px 0px'
+                    }}
                     defaultValue="Round trip"
-                    startAdornment={<SwapHorizIcon sx={{ mr: 1 }} />}
+                    startAdornment={<SwapHorizIcon sx={{ mr: 1, ml: 0}} />}
                     MenuProps={{
                         PaperProps: {
                             sx: {
@@ -42,18 +58,31 @@ export default function FlightSearchBar() {
                         display: "flex",
                         alignItems: "center",
                         pt: 1,
+                        width: '100%'
                     }}
                 >
 
                     {/* From Input */}
+    
                     <TextField
                         placeholder="Where from?"
-                        slots={{
-                            startAdornment: (
-                                <InputAdornment position="start">
-                                    <RadioButtonCheckedIcon />
-                                </InputAdornment>
-                            ),
+                        slotProps={{
+                            input: {        
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <CircleIconOutlined 
+                                        sx={{
+                                            color: theme.palette.grey[600],
+                                            fontSize: '14px',
+                                            mr: 1,
+                                            ml: 0,
+                                        }}/>
+                                    </InputAdornment>
+                                ),
+                            },
+                        }}
+                        sx={{
+                            flex:1
                         }}
                     />
 
@@ -65,30 +94,79 @@ export default function FlightSearchBar() {
                     {/* To Input */}
                     <TextField
                         placeholder="Where to?"
-                        slots={{
-                            startAdornment: (
-                                <InputAdornment position="start">
-                                    <LocationOnIcon />
-                                </InputAdornment>
-                            ),
+                        slotProps={{
+                            input: {
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <LocationOnIconOutlined />
+                                    </InputAdornment>
+                                ),
+                            },
                         }}
-
+                        sx={{
+                            flex:1
+                        }}
                     />
 
                     {/* Dates */}
                     <TextField
-                        placeholder="Wed, Jan 1"
-                        slots={{
-                            startAdornment: (
-                                <InputAdornment position="start">
-                                    <CalendarTodayIcon />
-                                </InputAdornment>
-                            ),
+                        placeholder="Departure"
+                        onMouseEnter={() => setIsDateFieldActive(true)}
+                        onMouseLeave={() => setIsDateFieldActive(false)}
+                        onFocus={() => setIsDateFieldActive(true)}
+                        onBlur={() => setIsDateFieldActive(false)}
+                        slotProps={{
+                            input: {
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <CalendarTodayIcon
+                                        sx={{
+                                            color: theme.palette.grey[600],
+                                            fontSize: '20px',
+                                            mr: 1,
+                                            ml: 0,
+                                        }}
+                                         />
+                                    </InputAdornment>
+                                ),
+                            },
                         }}
-                        sx={{ minWidth: 140 }}
+                        sx={{
+                            pl: '15px',
+                            '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
+                                borderRight: 'none',
+                                borderRadius: '10px 0px 0px 10px'
+                            },
+                            '& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline': {
+                                borderRight: '1px solid',
+                                borderRadius: '10px'
+                            },
+                            '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                                borderRight: `2px solid ${theme.palette.common.blue}`,
+                                borderRadius: '10px'
+                            }
+                        }}
                     />
+                    <Box sx={{
+                        display: isDateFieldActive ? 'none' : 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        height: '54px',
+                        borderTop: `1px solid ${theme.palette.grey[400]}`,
+                        borderBottom: `1px solid ${theme.palette.grey[400]}`,
+                    }}>
+                        <Divider orientation="vertical" flexItem sx={{ 
+                            height: '27px', 
+                            alignSelf: 'center',
+                            backgroundColor: theme.palette.grey[300],
+                        }}/>
+                    </Box>
                     <TextField
-                        placeholder="Fri, Jan 31"
+                        placeholder="Return"
+                        onMouseEnter={() => setIsDateFieldActive(true)}
+                        onMouseLeave={() => setIsDateFieldActive(false)}
+                        onFocus={() => setIsDateFieldActive(true)}
+                        onBlur={() => setIsDateFieldActive(false)}
                         slots={{
                             startAdornment: (
                                 <InputAdornment position="start">
@@ -96,7 +174,21 @@ export default function FlightSearchBar() {
                                 </InputAdornment>
                             ),
                         }}
-                        sx={{ minWidth: 140 }}
+                        sx={{
+                            flex:1,
+                            '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
+                                borderLeft: 'none',
+                                borderRadius: '0px 10px 10px 0px'
+                            },
+                            '& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline': {
+                                borderLeft: '1px solid',
+                                borderRadius: '10px'
+                            },
+                            '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                                borderLeft: `2px solid ${theme.palette.common.blue}`,
+                                borderRadius: '10px'
+                            }
+                        }}
                     />
 
                     {/* Search Button */}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,6 +6,7 @@ import Box from '@mui/material/Box';
 import { ReactComponent as Flights } from '../images/svg/flights.svg';
 import Typography from '@mui/material/Typography';
 import FlightSearchBar from '../components/searchBar/FlightsSearchBar';
+import InfoIconOutlined from '@mui/icons-material/InfoOutlined';
 const HomePage = () => {
   return (
     <MainLayout>
@@ -38,9 +39,30 @@ const HomePage = () => {
             >
                 Flights
             </Typography>
+           
         </Box>
         <FlightSearchBar />
+          <Box sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'left',
+          width: '100%',
+        }}>
+
+          <Typography variant="h6"
+              sx={{
+                  textAlign: 'left',
+                  mr: '4rem',
+                  ml: '4rem',
+                  mt: 6,
+              }}>
+                      Find cheap flights from your location to anywhere
+                      <InfoIconOutlined sx={{ fontSize: '20px', ml: 1, color: theme.palette.grey[600] }} />
+          </Typography>
+        </Box>
+
       </Box>
+     
     </MainLayout>
   );
 };
